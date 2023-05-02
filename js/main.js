60add223fdb9375975916033c928e7c870f4ea20
Alexey_Makeev <makeevhome@mail.ru>
2023-05-02 16:17:37 +0300

    add adaptive site

--- js/main.js
+++ js/main.js
@@ -1,26 +1,32 @@
-
-const tabItems = document.querySelectorAll('.tabs__btn-item');
-const tabContents = document.querySelectorAll('.tabs__content-item');
+const tabItems = document.querySelectorAll(".tabs__btn-item");
+const tabContents = document.querySelectorAll(".tabs__content-item");
 
 tabItems.forEach((element) => {
-  element.addEventListener('click', openHandler);
-})
+  element.addEventListener("click", openHandler);
+});
 
-const openHandler = (evt) => {
+function openHandler(evt) {
   const tabTarget = evt.currentTarget;
   const button = tabTarget.dataset.button;
 
   tabItems.forEach((item) => {
-    item.classList.remove('tabs__btn-item--active');
+    item.classList.remove("tabs__btn-item--active");
   });
 
-  tabTarget.classList.add('tabs__btn-item--active');
+  tabTarget.classList.add("tabs__btn-item--active");
 
   tabContents.forEach((item) => {
-    item.classList.remove('tabs__content-item--active');
+    item.classList.remove("tabs__content-item--active");
   });
-  document.getElementById(button).classList.add('tabs__content-item--active');
+  document.getElementById(button).classList.add("tabs__content-item--active");
 }
+
+const menuBtn = document.querySelector(".menu__btn");
+const menu = document.querySelector(".menu__list");
+
+menuBtn.addEventListener("click", () => {
+  menu.classList.toggle("menu__list--active");
+});
 
 const swiper = new Swiper(".swiper", {
   effect: "fade",
@@ -32,4 +38,3 @@ const swiper = new Swiper(".swiper", {
     disableOnInteraction: false,
   },
 });
-
